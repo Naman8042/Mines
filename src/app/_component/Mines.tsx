64f40daab945500df5fe20979053527c05b3c17d
Naman8042 <131790382+Naman8042@users.noreply.github.com>
2024-06-28 12:08:28 +0530

    12:08

--- src/app/_component/Mines.tsx
+++ src/app/_component/Mines.tsx
@@ -47,39 +47,32 @@ const Mines = ({minesRatio,setDiamonds,diamonds,generateDiamondsAndMinesArray,in
 
   return (
     <div className="md:w-[75%] h-full p-5 md:p-0 mt-[5%] md:mt-0 flex bg-gray-700 justify-center items-center rounded-r-md">
-      
-        <div className="grid grid-cols-4 gap-2 ">
-        {diamonds.map((diamond,index)=>(
-            
-                included.includes(index)?(
-                    <div id={`${index}`} className="bg-gray-800 flex justify-center items-center rounded-xl w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32">
-                    {
-                        diamond==='diamond'?(
-                            <IoDiamond style={{ color: '#32CD32' }} size={50}/>
-                        ):(
-                        <FaBomb size={50} color="red" />
-
-                        )
-                    }
-                    
-                    </div>
-                    
-                ):(
-                    
-                    <div
+      <div className="grid grid-cols-4 gap-2 ">
+        {diamonds.map((item, index) => (
+          included.includes(index) ? (
+            <div
               key={index}
-              onClick={() => diamond === 'mine' ? addAll() : Add(index)}
-              className="bg-gray-500 w-16 h-16 md:w-24 md:h-24 lg:w-32 lg:h-32 hover:-translate-y-1 rounded-xl"
+              onClick={addAll}
+              className="bg-gray-800 flex justify-center items-center rounded-xl w-32 h-32"
             >
-              <div className="bg-gray-400 hover:bg-gray-200 md:w-24 w-16 lg:w-32 shadow-2xl h-[93%] rounded-xl" />
+              {item === 'diamond' ? (
+                <IoDiamond style={{ color: '#32CD32' }} size={50} />
+              ) : (
+                <FaBomb size={50} color="red" />
+              )}
+            </div>
+          ) : (
+            <div
+              key={index}
+              onClick={() => item === 'mine' ? addAll() : Add(index)}
+              className="bg-gray-500 h-32 w-32 hover:-translate-y-1 rounded-xl"
+            >
+              <div className="bg-gray-400 hover:bg-gray-200 w-32 shadow-2xl h-[93%] rounded-xl" />
               <div className="h-[7%]" />
             </div>
-                )
-            
-        ))
-        }
-        </div>
-      
+          )
+        ))}
+      </div>
     </div>
   )
 }
